7a9f064a4595f5f4375e45f2f3042851d0954d9e
Sean Murphy <sean@iamseanmurphy.com>
2026-03-05 09:26:22 +0000

    3.2.3

--- notificationx/assets/admin/js/admin.asset.php
+++ notificationx/assets/admin/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '39cec3ba028550eaa18a');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => 'd7f0511eefc1a4aed838');
